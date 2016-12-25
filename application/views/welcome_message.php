75b70843d952a0ec66a2918821b03ac5d4336b7c
bb2ebb <bb2ebb@gmail.com>
2016-12-26 05:20:05 +0700

    v1

---
+++ application/views/welcome_message.php
@@ -0,0 +1,148 @@
+<html lang="en" class="no-js"> <!--<![endif]-->  
+    <head>
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Screen - Creative HTML Template</title>
+        <link rel="stylesheet" href="../../assets/1hmscr/css/bootstrap.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Merriweather:400,700,900,300' rel='stylesheet' type='text/css'>
+        <!-- animate CSS -->
+        <link rel="stylesheet" href="../../assets/1hmscr/css/animate.css">	
+        <!-- main CSS -->
+        <link rel="stylesheet" href="../../assets/1hmscr/css/style.css">
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+
+        <div class="preloader">
+            <div class="status">
+                <div class="status-mes"></div>
+            </div>
+        </div>
+
+        <!-- Begin Header -->
+        <header>
+            <div class="text-center head-title">
+                <img src="img/logo.png" alt="" />
+                <h2><span>Sc</span>reen - Creative HTML Template</h2>
+            </div>
+        </header>
+        <!-- //End Header -->
+
+        <!-- START DEMO-->
+        <section class="demo">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/1.jpg" class="img-responsive" alt="" />
+                                <h3>Home Default</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_parallax.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/2.jpg" class="img-responsive"  alt="" />
+                                <h3>Home Parallax</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_slideshow.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/3.jpg" class="img-responsive"  alt="" />
+                                <h3>Home Slideshow</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_text_slider.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/4.jpg" class="img-responsive"  alt="" />
+                                <h3>Home Text Slider</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_text_rotator.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/5.jpg" class="img-responsive"  alt="" />
+                                <h3>Home Text Rotator</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_htmlvideo.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/6.jpg" class="img-responsive"  alt="" />
+                                <h3>Home HTML5 Video</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_youtube.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/7.jpg" class="img-responsive"  alt="" />
+                                <h3>Home Youtube</h3>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                        <div class="demo_img">
+                            <a href="screen/index_without_map.html" target="_blank">
+                                <img src="../../assets/1hmscr/img/8.jpg" class="img-responsive"  alt="" />
+                                <h3>Without Google map</h3>
+                            </a>
+                        </div>
+                    </div>				
+                </div><!--- END ROW -->								
+            </div><!--- END CONTAINER -->
+        </section>
+        <!-- END DEMO -->
+
+        <!-- START FOOTER -->
+        <footer>
+            <div class="text-center">
+                <p class="copyright">
+                    Screen &copy; 2016. All Rights Reserved.
+                </p>
+            </div>
+        </footer>
+        <!-- END FOOTER -->
+
+        <!-- jQuery -->
+        <script src="../../assets/1hmscr/js/jquery-1.11.3.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="../../assets/1hmscr/js/bootstrap.min.js"></script>
+        <!-- WOW - Reveal Animations When You Scroll -->
+        <script src="../../assets/1hmscr/js/wow.min.js"></script>
+
+        <script type="text/javascript">
+            /*preloader*/
+            $(window).load(function () {
+                $('.status').fadeOut();
+                $('.preloader').delay(350).fadeOut('slow');
+            });
+            /*End preloader*/
+        </script>
+
+        <script type="text/javascript">
+            /*wow animation*/
+            new WOW().init();
+            /*End wow animation*/
+        </script>
+
+    </body>
+
+</html>
+<!--http://themesvila.com/html-templates/preview-screen/-->
