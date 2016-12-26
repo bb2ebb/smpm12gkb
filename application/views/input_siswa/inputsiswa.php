caeb7f6f859cf5430580e0715e6eba0ac40b934f
bb2ebb <bb2ebb@gmail.com>
2016-12-26 11:22:17 +0700

    v1

---
+++ application/views/input_siswa/inputsiswa.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap Login Form Template</title>
+        <link rel="stylesheet" href="./assets/2inputsiswa/css.css">
+        <link rel="stylesheet" href="./assets/2inputsiswa/bootstrap.css">
+        <link rel="stylesheet" href="./assets/2inputsiswa/font-awesome.css">
+        <link rel="stylesheet" href="./assets/2inputsiswa/form-elements.css">
+        <link rel="stylesheet" href="./assets/2inputsiswa/style.css">
+        <link rel="shortcut icon" href="http://azmind.com/demo/bootstrap-login-forms/form-1/assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://azmind.com/demo/bootstrap-login-forms/form-1/assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://azmind.com/demo/bootstrap-login-forms/form-1/assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://azmind.com/demo/bootstrap-login-forms/form-1/assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="http://azmind.com/demo/bootstrap-login-forms/form-1/assets/ico/apple-touch-icon-57-precomposed.png">
+
+    </head>
+
+    <body>
+
+        <!-- Top content -->
+        <div class="top-content">
+
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h1><strong>Bootstrap</strong> Login Form</h1>
+                            <div class="description">
+                                <p>
+                                    This is a free responsive login form made with Bootstrap. 
+                                    Download it on <a href="http://azmind.com/"><strong>AZMIND</strong></a>, customize and use it as you like!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                            <div class="form-top">
+                                <div class="form-top-left">
+                                    <h3>Login to our site</h3>
+                                    <p>Enter your username and password to log on:</p>
+                                </div>
+                                <div class="form-top-right">
+                                    <i class="fa fa-key"></i>
+                                </div>
+                            </div>
+                            <div class="form-bottom">
+                                <form role="form" action="" method="post" class="login-form">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="form-username">Username</label>
+                                        <input name="form-username" placeholder="Username..." class="form-username form-control" id="form-username" type="text">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="form-password">Password</label>
+                                        <input name="form-password" placeholder="Password..." class="form-password form-control" id="form-password" type="password">
+                                    </div>
+                                    <button type="submit" class="btn">Sign in!</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+<!--                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 social-login">
+                            <h3>...or login with:</h3>
+                            <div class="social-login-buttons">
+                                <a class="btn btn-link-1 btn-link-1-facebook" href="#">
+                                    <i class="fa fa-facebook"></i> Facebook
+                                </a>
+                                <a class="btn btn-link-1 btn-link-1-twitter" href="#">
+                                    <i class="fa fa-twitter"></i> Twitter
+                                </a>
+                                <a class="btn btn-link-1 btn-link-1-google-plus" href="#">
+                                    <i class="fa fa-google-plus"></i> Google Plus
+                                </a>
+                            </div>
+                        </div>
+                    </div>-->
+                </div>
+            </div>
+
+        </div>
+        <!-- Javascript -->
+        <script src="./assets/2inputsiswa/jquery-1.js"></script>
+        <script src="./assets/2inputsiswa/bootstrap.js"></script>
+        <script src="./assets/2inputsiswa/jquery.js"></script>
+        <script src="./assets/2inputsiswa/scripts.js"></script>
+        <div class="backstretch" style="left: 0px; top: 0px; overflow: hidden; margin: 0px; padding: 0px; height: 611px; width: 1007px; z-index: -999999; position: fixed;"><img style="position: absolute; margin: 0px; padding: 0px; border: medium none; width: 1007px; height: 671.333px; max-height: none; max-width: none; z-index: -999999; left: 0px; top: -30.1667px;" src="./assets/2inputsiswa/1.jpg"></div>
+    </body>
+</html>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,106 +1,90 @@
-<html lang="en" class="no-js"> <!--<![endif]-->  
-    <head>
-        <!-- Meta -->
+<html class="no-js" lang="en"><head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Screen - Creative HTML Template</title>
-        <link rel="stylesheet" href="../../assets/1hmscr/css/bootstrap.min.css">
-        <link href='https://fonts.googleapis.com/css?family=Merriweather:400,700,900,300' rel='stylesheet' type='text/css'>
-        <!-- animate CSS -->
-        <link rel="stylesheet" href="../../assets/1hmscr/css/animate.css">	
-        <!-- main CSS -->
-        <link rel="stylesheet" href="../../assets/1hmscr/css/style.css">
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        <link rel="stylesheet" href="./assets/1hmscr/css/bootstrap.css">
+        <link href="./assets/1hmscr/css/css.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="./assets/1hmscr/css/animate.css">	
+        <link rel="stylesheet" href="./assets/1hmscr/css/style.css">
     </head>
 
     <body>
-
-
-        <div class="preloader">
-            <div class="status">
+        <div class="preloader" style="display: none;">
+            <div class="status" style="display: none;">
                 <div class="status-mes"></div>
             </div>
         </div>
-
-        <!-- Begin Header -->
         <header>
             <div class="text-center head-title">
-                <img src="img/logo.png" alt="" />
-                <h2><span>Sc</span>reen - Creative HTML Template</h2>
+                <img src="./assets/1hmscr/img/logo.png" alt="">
+                <h2><span>SMP</span> Muhammadiyah 12 GKB Gresik</h2>
             </div>
         </header>
-        <!-- //End Header -->
-
-        <!-- START DEMO-->
         <section class="demo">
             <div class="container">
                 <div class="row text-center">
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/1.jpg" class="img-responsive" alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index.html" target="_blank">
+                                <img src="./assets/1hmscr/img/1.jpg" class="img-responsive" alt="">
                                 <h3>Home Default</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_parallax.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/2.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_parallax.html" target="_blank">
+                                <img src="./assets/1hmscr/img/2.jpg" class="img-responsive" alt="">
                                 <h3>Home Parallax</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_slideshow.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/3.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_slideshow.html" target="_blank">
+                                <img src="./assets/1hmscr/img/3.jpg" class="img-responsive" alt="">
                                 <h3>Home Slideshow</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_text_slider.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/4.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_text_slider.html" target="_blank">
+                                <img src="./assets/1hmscr/img/4.jpg" class="img-responsive" alt="">
                                 <h3>Home Text Slider</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_text_rotator.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/5.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_text_rotator.html" target="_blank">
+                                <img src="./assets/1hmscr/img/5.jpg" class="img-responsive" alt="">
                                 <h3>Home Text Rotator</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_htmlvideo.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/6.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_htmlvideo.html" target="_blank">
+                                <img src="./assets/1hmscr/img/6.jpg" class="img-responsive" alt="">
                                 <h3>Home HTML5 Video</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_youtube.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/7.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_youtube.html" target="_blank">
+                                <img src="./assets/1hmscr/img/7.jpg" class="img-responsive" alt="">
                                 <h3>Home Youtube</h3>
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn">
+                    <div class="col-md-4 col-sm-4 col-xs-12 wow zoomIn animated" style="visibility: visible; animation-name: zoomIn;">
                         <div class="demo_img">
-                            <a href="screen/index_without_map.html" target="_blank">
-                                <img src="../../assets/1hmscr/img/8.jpg" class="img-responsive"  alt="" />
+                            <a href="http://themesvila.com/html-templates/preview-screen/screen/index_without_map.html" target="_blank">
+                                <img src="./assets/1hmscr/img/8.jpg" class="img-responsive" alt="">
                                 <h3>Without Google map</h3>
                             </a>
                         </div>
@@ -114,18 +98,18 @@
         <footer>
             <div class="text-center">
                 <p class="copyright">
-                    Screen &copy; 2016. All Rights Reserved.
+                    Screen © 2016. All Rights Reserved.
                 </p>
             </div>
         </footer>
         <!-- END FOOTER -->
 
         <!-- jQuery -->
-        <script src="../../assets/1hmscr/js/jquery-1.11.3.min.js"></script>
+        <script src="./assets/1hmscr/js/jquery-1.js"></script>
         <!-- Latest compiled and minified JavaScript -->
-        <script src="../../assets/1hmscr/js/bootstrap.min.js"></script>
+        <script src="./assets/1hmscr/js/bootstrap.js"></script>
         <!-- WOW - Reveal Animations When You Scroll -->
-        <script src="../../assets/1hmscr/js/wow.min.js"></script>
+        <script src="./assets/1hmscr/js/wow.js"></script>
 
         <script type="text/javascript">
             /*preloader*/
@@ -135,14 +119,10 @@
             });
             /*End preloader*/
         </script>
-
         <script type="text/javascript">
             /*wow animation*/
             new WOW().init();
             /*End wow animation*/
         </script>
-
     </body>
-
 </html>
-<!--http://themesvila.com/html-templates/preview-screen/-->
